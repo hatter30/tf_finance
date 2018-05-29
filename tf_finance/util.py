fce4a1b0b5e390097b7cea4949de80a2ec54a972
oh june <tic.for.tac@gmail.com>
2018-05-29 21:24:36 +0900

    add daily_sise

--- tf_finance/util.py
+++ tf_finance/util.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+import urllib.request
+from bs4 import BeautifulSoup
 
 def get_corp_code(from_url=False, file_name='corp_code.xlsx'):
     if from_url:
@@ -21,9 +23,27 @@ def get_etf_code(file_name = 'etf_code.xlsx'):
 
 
 def get_finance_url(item_name, code_df):
-    code = code_df.query("name=='{}'".format(item_name))['code'].to_string(index=False)
-    url = 'http://finance.naver.com/item/sise_day.nhn?code={code}'.format(code=code)
+    if not item_name in code_df.name.values:
+        raise ValueError('Invalid item name %s' % item_name)
+    code = int(code_df.query("name=='{}'".format(item_name))['code'].to_string(index=False))
+    url = 'http://finance.naver.com/item/sise_day.nhn?code={code:06d}'.format(code=code)
     return url
+
+def daily_sise(url, num_start, num_end):
+    df = pd.DataFrame()
+    for page in range(num_start, num_end):
+        pg_url = '{url}&page={page}'.format(url=url, page=page)
+        df = df.append(pd.read_html(pg_url, header=0)[0], ignore_index=True)
+    df.dropna()
+    return df
+
+def get_last_page_num(url):
+    req = urllib.request.Request(url)
+    data = urllib.request.urlopen(req).read()
+    bs = BeautifulSoup(data, 'html.parser')
+    text_attrs = bs.find_all('a')[-1].attrs['href']
+    num_last_pg = int(text_attrs.split('=')[-1])
+    return num_last_pg
 
 
 if __name__ == '__main__':
