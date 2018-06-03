0c4ddaa244562d04f89a31815674bc83c5823b84
oh june <tic.for.tac@gmail.com>
2018-06-04 04:04:42 +0900

    add PriceManager

---
+++ tf_finance/price_manage.py
@@ -0,0 +1,57 @@
+
+import pandas as pd
+
+from tf_finance.util import *
+
+
+class PriceManager:
+    def __init__(self, names=['KODEX 200']):
+        self._etf_code = get_etf_code()
+
+        self._price = {name: self.load_from_web_csv(name) for name in names}
+        self.save_all_price()
+
+    def load_csv_to_df(self, name):
+        file_path = absolute_file_path + '\\price_data\\' + name + '.csv'
+        if os.path.exists(file_path):
+            df = pd.read_csv(file_path, sep='\t', encoding='utf-8')
+            df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+            return df
+        else:
+            return None
+
+    def load_from_web(self, name, load_pg_num=None):
+        url = get_finance_url(name, self._etf_code)
+        df = daily_sise(url, num_end=load_pg_num)
+        return df
+
+    def load_from_web_csv(self, name, load_pg_num=10):
+        old_df = self.load_csv_to_df(name)
+        if old_df is not None:
+            updated_df = self.load_from_web(name, load_pg_num)
+            return merge_sise_by_day(updated_df, old_df)
+        else:
+            return self.load_from_web(name)
+
+    def save_price(self, name, df):
+        file_path = absolute_file_path + '\\price_data\\' + name + '.csv'
+        df.to_csv(file_path, sep='\t', encoding='utf-8')
+
+    def save_all_price(self):
+        for name, price in self._price.items():
+            self.save_price(name, price)
+
+    def daily_price(self, names=['KODEX 200']):
+        day = self._price[names[0]]['날짜']
+        daily_price_list = [self._price[name]['종가'].rename(name) for name in names]
+        merged = pd.concat([day] + daily_price_list, axis=1)
+
+        format = '%Y.%m.%d'
+        merged['date'] = pd.DatetimeIndex(pd.to_datetime(merged['날짜'], format=format))
+        del merged['날짜']
+        merged = merged.set_index('date')
+
+        return merged
+
+    def __getitem__(self, key):
+        return self._price[key]
--- tf_finance/util.py
+++ tf_finance/util.py
@@ -3,11 +3,13 @@ import pandas as pd
 import urllib.request
 from bs4 import BeautifulSoup
 
+absolute_file_path = os.path.dirname(os.path.realpath(__file__))
+
 def get_corp_code(from_url=False, file_name='corp_code.xlsx'):
     if from_url:
         code_df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
     else:
-        file_path = os.path.dirname(os.path.realpath(__file__)) + '\\raw_data\\' + file_name
+        file_path = absolute_file_path + '\\raw_data\\' + file_name
         code_df = pd.read_excel(file_path)
 
     code_df.종목코드 = code_df.종목코드.map('{:06d}'.format)
@@ -23,15 +25,20 @@ def get_etf_code(file_name = 'etf_code.xlsx'):
 
 
 def get_finance_url(item_name, code_df):
-    if not item_name in code_df.name.values:
+    if not item_name in code_df.name_en.values:
         raise ValueError('Invalid item name %s' % item_name)
-    code = int(code_df.query("name=='{}'".format(item_name))['code'].to_string(index=False))
+    code = int(code_df.query("name_en=='{}'".format(item_name))['code'].to_string(index=False))
     url = 'http://finance.naver.com/item/sise_day.nhn?code={code:06d}'.format(code=code)
     return url
 
-def daily_sise(url, num_start, num_end):
+def daily_sise(url, num_start=1, num_end=None):
+    if not num_end:
+        last_pg_num = get_last_page_num(url)
+    else:
+        last_pg_num = num_end
+
     df = pd.DataFrame()
-    for page in range(num_start, num_end):
+    for page in range(num_start, last_pg_num):
         pg_url = '{url}&page={page}'.format(url=url, page=page)
         df = df.append(pd.read_html(pg_url, header=0)[0], ignore_index=True)
     df.dropna()
@@ -44,6 +51,17 @@ def get_last_page_num(url):
     text_attrs = bs.find_all('a')[-1].attrs['href']
     num_last_pg = int(text_attrs.split('=')[-1])
     return num_last_pg
+
+def merge_sise_by_day(updated_df, old_df, day_column='날짜'):
+    old_df_first_day = old_df.loc[0][day_column]
+    index_matched_list = updated_df.index[updated_df[day_column] == old_df_first_day].tolist()
+    assert len(index_matched_list) == 1, 'Invalid index list'
+    index_matched = index_matched_list[0]
+
+    sliced_update_df = updated_df.loc[:index_matched-1]
+    merged = pd.concat([sliced_update_df, old_df])
+    merged = merged.reset_index(drop=True)
+    return merged
 
 
 if __name__ == '__main__':
